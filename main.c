6fafdf54f6bbe38b9c9f2f718302098866f5b6ec
unknown <romain.teyssier@mail.mcgill.ca>
2024-09-05 16:05:04 -0400

    f

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
-
 int main() {
-  printf("Hello World!\n");
+    int goal_state[3][3]={{1,2,3},{8,0,4},{7,6,5}};
 
-  return 0;
+}
+
+typedef struct node {
+    int state[3][3] = NULL;
+    int blank_index = NULL;
+    char* moves[4] = NULL;
+    struct node* parent;
+}
+
+node legal_moves(node node) {
+    if (node.blank_index<2) {
+        node.moves[0]='up';
+    }
+    if (node.blank_index>=6) {
+        node.moves[0]='down';
+    } 
+    if (node.blank_index%3==2) {
+        node.moves[0]='right';
+    } 
+    if (node.blank_index%3==0) {
+        node.moves[0]='left';
+    }
+    return node;
+}
+
+action(node node,char* direction) {
+
+}
+
+expand(node node) {
+    char* direction in node.moves
 }
