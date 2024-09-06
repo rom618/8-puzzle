45cd02f064c1d2052291bfe06c2f08bcf0de0802
Vianpyro <vianney@veremme.org>
2024-09-06 11:10:43 -0400

    Use pointers

--- main.c
+++ main.c
@@ -14,27 +14,26 @@ typedef struct node {
 } node;
 
 // Function to determine legal moves based on the position of the blank tile
-node legal_moves(node n) {
+void legal_moves(node *n) {
     // Initialize moves to empty strings
-    strcpy(n.moves[0], "");
-    strcpy(n.moves[1], "");
-    strcpy(n.moves[2], "");
-    strcpy(n.moves[3], "");
+    strcpy(n->moves[0], "");
+    strcpy(n->moves[1], "");
+    strcpy(n->moves[2], "");
+    strcpy(n->moves[3], "");
 
     // Determine legal moves
-    if (n.blank_index > 2) {  // If blank is not in the top row, "up" is a valid move
-        strcpy(n.moves[0], "up");
+    if (n->blank_index > 2) {  // If blank is not in the top row, "up" is a valid move
+        strcpy(n->moves[0], "up");
     }
-    if (n.blank_index < 6) {  // If blank is not in the bottom row, "down" is a valid move
-        strcpy(n.moves[1], "down");
+    if (n->blank_index < 6) {  // If blank is not in the bottom row, "down" is a valid move
+        strcpy(n->moves[1], "down");
     }
-    if (n.blank_index % 3 < 2) {  // If blank is not in the rightmost column, "right" is a valid move
-        strcpy(n.moves[2], "right");
+    if (n->blank_index % 3 < 2) {  // If blank is not in the rightmost column, "right" is a valid move
+        strcpy(n->moves[2], "right");
     }
-    if (n.blank_index % 3 > 0) {  // If blank is not in the leftmost column, "left" is a valid move
-        strcpy(n.moves[3], "left");
+    if (n->blank_index % 3 > 0) {  // If blank is not in the leftmost column, "left" is a valid move
+        strcpy(n->moves[3], "left");
     }
-    return n;
 }
 
 // Function to perform an action based on the direction
@@ -56,16 +55,27 @@ int main() {
     node root = {{{1, 2, 3}, {8, 0, 4}, {7, 6, 5}}, 4, {}, NULL};
 
     // Determine legal moves for the initial state
-    root = legal_moves(root);
+    legal_moves(&root);
 
     // Test the legal moves by printing them
     for (int i = 0; i < 4; i++) {
-        if (strlen(root.moves[i]) > 0) {  // Print only if the move is not an empty string
+        if (root.moves[i][0] != '\0') {  // Print only if the move is not an empty string
             printf("Move %d: %s\n", i, root.moves[i]);
         }
     }
 
-    random_number_sequence(9);  // Random number sequence usage example
+    int sequence_size = 9;
+    int *array = (int *)malloc(sequence_size * sizeof(int));
+
+    random_number_sequence(array, sequence_size);  // Random number sequence usage example
+
+    // Print the shuffled array
+    for (int i = 0; i < sequence_size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    free(array);  // Free allocated memory
 
     return 0;
 }
