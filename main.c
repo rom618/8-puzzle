95e096c064230bb4bb45d5f77b78dc962305da68
Vianpyro <vianney@veremme.org>
2024-09-06 11:21:51 -0400

    Use function prototypes to keep main function visible

--- main.c
+++ main.c
@@ -21,6 +21,46 @@ typedef struct node {
     struct node *parent;  // Pointer to the parent node
 } node;
 
+// Function prototypes
+void legal_moves(node *n);
+void action(node *n, const char *direction);
+void expand(node *n);
+const char *direction_to_string(Direction dir);
+
+int main() {
+    srand(time(NULL));  // Seed the random number generator
+
+    int goal_state[3][3] = {{1, 2, 3}, {8, 0, 4}, {7, 6, 5}};
+
+    // Example of initializing a node
+    node root = {{{1, 2, 3}, {8, 0, 4}, {7, 6, 5}}, 4, {}, NULL};
+
+    // Determine legal moves for the initial state
+    legal_moves(&root);
+
+    // Test the legal moves by printing them
+    for (int i = 0; i < 4; i++) {
+        if (root.moves[i] != NONE) {
+            printf("Move %d: %s\n", i, direction_to_string(root.moves[i]));
+        }
+    }
+
+    int sequence_size = 9;
+    int *array = (int *)malloc(sequence_size * sizeof(int));
+
+    random_number_sequence(array, sequence_size);  // Random number sequence usage example
+
+    // Print the shuffled array
+    for (int i = 0; i < sequence_size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    free(array);  // Free allocated memory
+
+    return 0;
+}
+
 // Function to determine legal moves based on the position of the blank tile
 void legal_moves(node *n) {
     // Initialize moves to NONE
@@ -53,36 +93,17 @@ void expand(node *n) {
     // Implement the expand function here
 }
 
-int main() {
-    srand(time(NULL));  // Seed the random number generator
-
-    int goal_state[3][3] = {{1, 2, 3}, {8, 0, 4}, {7, 6, 5}};
-
-    // Example of initializing a node
-    node root = {{{1, 2, 3}, {8, 0, 4}, {7, 6, 5}}, 4, {}, NULL};
-
-    // Determine legal moves for the initial state
-    legal_moves(&root);
-
-    // Test the legal moves by printing them
-    for (int i = 0; i < 4; i++) {
-        if (root.moves[i][0] != '\0') {  // Print only if the move is not an empty string
-            printf("Move %d: %s\n", i, root.moves[i]);
-        }
+const char *direction_to_string(Direction dir) {
+    switch (dir) {
+        case UP:
+            return "up";
+        case DOWN:
+            return "down";
+        case LEFT:
+            return "left";
+        case RIGHT:
+            return "right";
+        default:
+            return "";
     }
-
-    int sequence_size = 9;
-    int *array = (int *)malloc(sequence_size * sizeof(int));
-
-    random_number_sequence(array, sequence_size);  // Random number sequence usage example
-
-    // Print the shuffled array
-    for (int i = 0; i < sequence_size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    free(array);  // Free allocated memory
-
-    return 0;
 }
