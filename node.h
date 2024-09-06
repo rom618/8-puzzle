393efc71a5d7e9fee56fa3ea006965203e911aab
Vianpyro <vianney@veremme.org>
2024-09-06 15:28:38 -0400

    Make the node puzzle state a const

--- node.h
+++ node.h
@@ -5,10 +5,10 @@
 
 // Define the node structure
 typedef struct node {
-    int state[3][3];      // 2D array to represent the puzzle state
-    int blank_index;      // Index of the blank (zero) tile
-    Direction moves[4];   // Array to store possible moves
-    struct node *parent;  // Pointer to the parent node
+    const int state[3][3];  // 2D array to represent the puzzle state
+    int blank_index;        // Index of the blank (zero) tile
+    Direction moves[4];     // Array to store possible moves
+    struct node *parent;    // Pointer to the parent node
 } node;
 
 // Function prototypes
